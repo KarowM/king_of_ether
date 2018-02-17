0364f97584988f93daf3bf061c90e8ad4a7fb7e1
Karow <karow.maruf14@imperial.ac.uk>
2018-02-17 18:19:54 +0000

    - added names for kings - update king of ether states upon new king election

--- KingOfEther.sol
+++ KingOfEther.sol
@@ -1,17 +1,23 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.20;
 
 contract KingOfEther {
     address private currentKing;
     uint private currentPrice = 0;
+    mapping (address => string) private kingNames;
 
-    function offerForThrone() external payable returns (bool)
+    function offerForThrone(string name) external payable returns (bool)
     {
-        require(msg.value > currentPrice);
+        require (currentPrice < msg.value);
+
+        currentPrice = msg.value;
+        currentKing.transfer(msg.value - currentPrice);
         currentKing = msg.sender;
+        kingNames[msg.sender] = name;
+        return true;
     }
 
-    function getCurrentKing() public view returns (address)
+    function getCurrentKing() public view returns (string)
     {
-        return currentKing;
+        return kingNames[currentKing];
     }
 }
