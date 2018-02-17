96dcd7baf51e7de6edaf175c631375e7b99236e4
Karow <karow.maruf14@imperial.ac.uk>
2018-02-17 17:57:54 +0000

    created function for offering to become king and getting current king's address

--- KingOfEther.sol
+++ KingOfEther.sol
@@ -1,6 +1,17 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.18;
 
 contract KingOfEther {
     address private currentKing;
     uint private currentPrice = 0;
+
+    function offerForThrone() external payable returns (bool)
+    {
+        require(msg.value > currentPrice);
+        currentKing = msg.sender;
+    }
+
+    function getCurrentKing() public view returns (address)
+    {
+        return currentKing;
+    }
 }
